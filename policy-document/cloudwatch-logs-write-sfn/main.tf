978d139832bf82f03c080eb369ad7aea915ec2de
SebRamsland <mail@sebastianramsland.com>
2024-10-25 14:06:21 +0200

    Add explicit cloudwatch logs write policy for step functions

---
+++ policy-document/cloudwatch-logs-write-sfn/main.tf
@@ -0,0 +1,18 @@
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogDelivery",
+      "logs:CreateLogStream",
+      "logs:GetLogDelivery",
+      "logs:UpdateLogDelivery",
+      "logs:DeleteLogDelivery",
+      "logs:ListLogDeliveries",
+      "logs:PutLogEvents",
+      "logs:PutResourcePolicy",
+      "logs:DescribeResourcePolicies",
+      "logs:DescribeLogGroups",
+    ]
+    resources = ["*"]
+  }
+}
